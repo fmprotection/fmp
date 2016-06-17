3f4d852d164aa95cbfe81731586f04ae0bec345a
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-17 09:55:17 -0500

    completed the edit profile form. Next step is to finish the controller method for it

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -3,25 +3,35 @@
 <div class="col-lg-8 col-lg-offset-1 col-md-8 col-md-offset-1 col-sm-8 col-sm-offset-1 col-xs-8 col-xs-offset-1">
 	<form class="form-horizontal" method="POST" action="{{{action('UsersController@update')}}}">
 		<div class="form-group">
-			<input type="text" name="fullname" id="fullname" value="$user->fullname">
+			<label for="fullname">Full Name</label>
+			<input type="text" name="fullname" id="fullname" value="{{{$user->fullname}}}">
 		</div>
 		<div class="form-group">
-			<input type="text" name="" id="" value="">
+			<label for="age">Age</label>
+			<input type="text" name="age" id="age" value="{{{$user->age}}}">
 		</div>
 		<div class="form-group">
-			<input type="text" name="" id="" value="">
+			<label for="gender">Gender</label>
+			<input type="text" name="gender" id="gender" value="{{{$user->gender}}}">
 		</div>
 		<div class="form-group">
-			<input type="text" name="" id="" value="">
+			<label for="weight">Weight</label>
+			<input type="text" name="weight" id="weight" value="{{{$user->weight}}}">
 		</div>
 		<div class="form-group">
-			<input type="text" name="" id="" value="">
+			<label for="height">Height</label>
+			<input type="text" name="height" id="height" value="{{{$user->height}}}">
 		</div>
 		<div class="form-group">
-			<input type="text" name="" id="" value="">
+			<label for="email">Email</label>
+			<input type="text" name="email" id="email" value="{{{$user->email}}}">
 		</div>
 		<div class="form-group">
-			<input type="text" name="" id="" value="">
+			<label for="phone">Phone</label>
+			<input type="text" name="phone" id="phone" value="{{{$user->phone}}}">
+		</div>
+		<div class="form-group">
+			<button class="btn btn-primary">Edit</button>
 		</div>
 	</form>
 </div>
